d21842c3ca9228f5275056898d01c9923b7c1fd9
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-10-31 09:02:16 +0700

    Membuat drawer pada tampilan awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:mytpqmobile/home.dart';
+void main() => runApp(MaterialApp(
+      home: MyTPQ(),
+    ));
 
-void main() => runApp(const MyTPQLogin());
-
-class MyTPQLogin extends StatelessWidget {
-  const MyTPQLogin({Key? key}) : super(key: key);
+class MyTPQ extends StatefulWidget {
+  const MyTPQ({Key? key}) : super(key: key);
 
   @override
+  State<MyTPQ> createState() => _MyTPQState();
+}
+
+class _MyTPQState extends State<MyTPQ> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login Page',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aplikasi MyTPQ"),
+        backgroundColor: Colors.blue[900],
       ),
-      home: const MyHome(),
+      backgroundColor: Colors.blue[300],
+
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Profil Akun"),
+            ),
+            ListTile(
+              leading: Icon(Icons.key),
+              title: Text("Ubah Kata Sandi"),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text("Tentang"),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("Keluar"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
