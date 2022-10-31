b40c1615163a4433c77b44c68f48c31e11d967f7
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-10-31 07:18:52 +0700

    Menambah stateless widget

---
+++ lib/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mytpqmobile/home.dart';
 
+class MyTPQLogin extends StatelessWidget {
+  const MyTPQLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Page',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white
+      ),
+      home: const MyHome(),
+    );
+  }
+}
