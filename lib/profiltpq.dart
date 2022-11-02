54cfc298a7f40afaf3caa2cad174e35f878a25aa
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-11-02 07:52:24 +0700

    Menambah informasi profil TPQ

--- lib/profiltpq.dart
+++ lib/profiltpq.dart
@@ -11,19 +11,47 @@ class _MyTpqProfilState extends State<MyTpqProfil> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Profil TPQ"),),
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Card(
-          child: Center(
-            child: Column(
-              children: const <Widget>[
-                Text("TPQ Darul Falihin"),
-                Text("Lokasi : Jl. Manukan Yoso, Tandes, Surabaya"),
-              ],
+        appBar: AppBar(
+          title: const Text("Profil TPQ"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: Card(
+            child: Center(
+              child: Column(
+                children: const <Widget>[
+                  Text("TPQ Darul Falihin"),
+                  Text("Berdiri : 2006"),
+                  Text("No. Statistik : 411.2.35.78.0445"),
+                  Text("Kepala TPQ : Farid Mashudi S.Pd.I."),
+                  Text("Metode pengajaran : An-Nahdliyah"),
+                  Text(
+                    "Metode ini terbukti sangat efektif, efisien, mudah dipahami sehingga memudahkan belajar membaca Al-Quran dengan benar.",
+                    textAlign: TextAlign.center,
+                  ),
+                  Text("Visi : "),
+                  Text(
+                    "Terciptanya generasi Qur'ani, berakhlakul karimah, berprestasi, berdasarkan IMTAQ dan bermanfaat bagi umat.",
+                    textAlign: TextAlign.center,
+                  ),
+                  Text("Misi : "),
+                  Text("1. Mengajarkan bacaan Al-Quran sesuai ilmu tajwid.", textAlign: TextAlign.center,),
+                  Text(
+                      "2. Membekali santri lebih memperdalam ajaran Islam pada jenjang selanjutnya.", textAlign: TextAlign.center,),
+                  Text(
+                    "3. Menanamkan sikap keimanan, keislaman, ketaqwaan sejak dini serta membiasakan santri berperilaku baik, sopan terhadap guru, orang tua, dan sesama.",
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                      "4. Melestarikan tradisi ilmiah dan amaliah salafus shalih.", textAlign: TextAlign.center,),
+                  Text(
+                    "5. Meningkatkan minat dan bakat santri dalam berkompetisi untuk meningkatkan prestasi.",
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-    ));
+        ));
   }
 }
