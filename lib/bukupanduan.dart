6f6288bebe2556824a5cb6400f513a80564b4526
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-11-08 13:11:34 +0700

    Tambah file bukupanduan.dart

---
+++ lib/bukupanduan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(bukuPanduan());
+}
+
+class bukuPanduan extends StatelessWidget {
+  const bukuPanduan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
