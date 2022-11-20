f80386385816e53949f0a9e9d507ef04c2d11e86
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-11-21 06:31:08 +0700

    Ubah jenis textfield

--- lib/spp.dart
+++ lib/spp.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
@@ -5,7 +7,7 @@ import 'package:open_file/open_file.dart';
 import 'package:pdf/pdf.dart' as pd;
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path_provider/path_provider.dart';
-import 'package:input_form_field/input_form_field.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class BayarSPP extends StatefulWidget {
   const BayarSPP({Key? key}) : super(key: key);
@@ -18,9 +20,13 @@ class BayarSPP extends StatefulWidget {
 class _bayarSPPState extends State<BayarSPP> {
   TextEditingController controllerNamaTerima = TextEditingController();
   TextEditingController controllerNamaSantri = TextEditingController();
+  TextEditingController controllerNoTelp = TextEditingController();
   TextEditingController controllerJenisByr = TextEditingController();
   TextEditingController controllerJumlahByr = TextEditingController();
   TextEditingController controllerBln = TextEditingController();
+
+  final phoneFormatter = MaskTextInputFormatter(
+      mask: '(+##) ###-####-####', type: MaskAutoCompletionType.lazy);
 
   @override
   Widget build(BuildContext context) {
@@ -54,176 +60,198 @@ class _bayarSPPState extends State<BayarSPP> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: ListView(
-          children: <Widget>[
-            Column(children: <Widget>[
-              const Padding(padding: EdgeInsets.all(10.0)),
-              InputFormField(
-                textEditingController: controllerNamaTerima,
-                labelText: "Penerima",
-                hintText: "Masukkan nama penerima (huruf kapital)",
-                hintTextStyle: const TextStyle(fontSize: 15),
-                borderColor: Colors.blue,
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                borderRadius: BorderRadius.circular(10),
-                errorPadding: const EdgeInsets.only(left: 10, top: 10),
-                validator: (v) {
-                  if (v != null && v.isEmpty) {
-                    return "Required";
+        child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            const Padding(padding: EdgeInsets.all(5.0)),
+            TextField(
+              autocorrect: true,
+              autofocus: true,
+              keyboardType: TextInputType.name,
+              enableInteractiveSelection: true,
+              controller: controllerNamaTerima,
+              decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  labelText: "Penerima",
+                  labelStyle: const TextStyle(fontSize: 15),
+                  hintText: "Masukkan nama penerima",
+                  hintStyle: const TextStyle(fontSize: 15),
+                  prefixIcon: const Icon(Icons.person)),
+            ),
+            const Padding(padding: EdgeInsets.all(5.0)),
+            TextField(
+              autocorrect: true,
+              autofocus: false,
+              keyboardType: TextInputType.name,
+              enableInteractiveSelection: true,
+              controller: controllerNamaSantri,
+              decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  labelText: "Santri",
+                  labelStyle: const TextStyle(fontSize: 15),
+                  hintText: "Masukkan nama santri",
+                  hintStyle: const TextStyle(fontSize: 15),
+                  prefixIcon: const Icon(Icons.person_add)),
+            ),
+            const Padding(padding: EdgeInsets.all(5.0)),
+            TextField(
+              inputFormatters: [phoneFormatter],
+              maxLength: 19,
+              autocorrect: true,
+              autofocus: false,
+              keyboardType: TextInputType.number,
+              enableInteractiveSelection: true,
+              controller: controllerNoTelp,
+              decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  labelText: "Nomor Telepon",
+                  labelStyle: const TextStyle(fontSize: 15),
+                  hintText: "Masukkan nomor telepon",
+                  hintStyle: const TextStyle(fontSize: 15),
+                  prefixIcon: const Icon(Icons.phone)),
+            ),
+            const Padding(padding: EdgeInsets.all(5.0)),
+            TextField(
+                autocorrect: true,
+                autofocus: false,
+                keyboardType: TextInputType.name,
+                enableInteractiveSelection: true,
+                controller: controllerJenisByr,
+                decoration: InputDecoration(
+                    fillColor: Colors.lightBlue[50],
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    labelText: "Jenis Pembayaran",
+                    labelStyle: const TextStyle(fontSize: 15),
+                    hintText: "Masukkan jenis pembayaran",
+                    hintStyle: const TextStyle(fontSize: 15),
+                    prefixIcon: const Icon(Icons.credit_card))),
+            const Padding(padding: EdgeInsets.all(5.0)),
+            TextField(
+              autocorrect: true,
+              autofocus: false,
+              keyboardType: TextInputType.number,
+              enableInteractiveSelection: true,
+              controller: controllerJumlahByr,
+              decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  labelText: "Jumlah Bayar",
+                  labelStyle: const TextStyle(fontSize: 15),
+                  hintText: "Masukkan jumlah bayar",
+                  hintStyle: const TextStyle(fontSize: 15),
+                  prefixIcon: const Icon(Icons.payments),
+                  prefixText: "Rp "),
+            ),
+            const Padding(padding: EdgeInsets.all(5.0)),
+            TextField(
+              autocorrect: true,
+              autofocus: false,
+              keyboardType: TextInputType.name,
+              enableInteractiveSelection: true,
+              controller: controllerBln,
+              decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  labelText: "Bulan",
+                  labelStyle: const TextStyle(fontSize: 15),
+                  hintText: "Masukkan bulan",
+                  hintStyle: const TextStyle(fontSize: 15),
+                  prefixIcon: const Icon(Icons.receipt)),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  var nmTerima = controllerNamaTerima.text;
+                  var nmSantri = controllerNamaSantri.text;
+                  var noTelp = controllerNoTelp.text;
+                  var jnsByr = controllerJenisByr.text;
+                  var jmlByr = controllerJumlahByr.text;
+                  var bln = controllerBln.text;
+                  void getPDF() async {
+                    final pdf = pw.Document();
+
+                    //buat page
+                    pdf.addPage(
+                      pw.Page(
+                        pageFormat: pd.PdfPageFormat.a4,
+                        build: (pw.Context context) {
+                          return pw.Column(
+                            children: [
+                              pw.Container(
+                                  alignment: pw.Alignment.center,
+                                  color: pd.PdfColors.green600,
+                                  width: double.infinity,
+                                  child: pw.Text("KWITANSI",
+                                      style: pw.TextStyle(
+                                          fontSize: 50,
+                                          fontWeight: pw.FontWeight.bold))),
+                              pw.Container(
+                                  alignment: pw.Alignment.center,
+                                  width: double.infinity,
+                                  child: pw.Text("${DateTime.now()} \n\n\n",
+                                      style: const pw.TextStyle(fontSize: 20))),
+                              pw.Container(
+                                  alignment: pw.Alignment.centerLeft,
+                                  width: double.infinity,
+                                  child: pw.Text(
+                                      "\n\n"
+                                      "Penerima : $nmTerima\n\n"
+                                      "Nama Santri : $nmSantri\n\n"
+                                      "Nomor Telepon : $noTelp\n\n"
+                                      "Jenis Pembayaran : $jnsByr\n\n"
+                                      "Jumlah Bayar : Rp$jmlByr\n\n"
+                                      "Bulan : $bln\n",
+                                      style: const pw.TextStyle(fontSize: 25))),
+                              pw.Container(
+                                  alignment: pw.Alignment.bottomRight,
+                                  width: double.infinity,
+                                  child: pw.Text(
+                                      "\n\n\n\n\n\n\n\n\nTTD \n"
+                                      "\n\n"
+                                      "($nmTerima)",
+                                      style: const pw.TextStyle(fontSize: 25))),
+                            ],
+                          );
+                        },
+                      ),
+                    ); // Page
+
+                    // simpan pdf
+                    Uint8List bytes = await pdf.save();
+
+                    //buat file kosong di directory
+                    final dir = await getApplicationDocumentsDirectory();
+                    final file = File('${dir.path}/myinvoice.pdf');
+
+                    //timpa file kosong
+                    await file.writeAsBytes(bytes);
+
+                    // buka pdf
+                    await OpenFile.open(file.path);
                   }
-                  return null;
+
+                  getPDF();
                 },
-              ),
-              const Padding(padding: EdgeInsets.all(10.0)),
-              InputFormField(
-                textEditingController: controllerNamaSantri,
-                labelText: "Nama Santri",
-                hintText: "Masukkan nama santri (huruf kapital)",
-                hintTextStyle: const TextStyle(fontSize: 15),
-                borderColor: Colors.blue,
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                borderRadius: BorderRadius.circular(10),
-                errorPadding: const EdgeInsets.only(left: 10, top: 10),
-                validator: (v) {
-                  if (v != null && v.isEmpty) {
-                    return "Required";
-                  }
-                  return null;
-                },
-              ),
-              const Padding(padding: EdgeInsets.all(10.0)),
-              InputFormField(
-                textEditingController: controllerJenisByr,
-                labelText: "Jenis Pembayaran",
-                hintText: "Masukkan jenis pembayaran",
-                hintTextStyle: const TextStyle(fontSize: 15),
-                borderColor: Colors.blue,
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                borderRadius: BorderRadius.circular(10),
-                errorPadding: const EdgeInsets.only(left: 10, top: 10),
-                validator: (v) {
-                  if (v != null && v.isEmpty) {
-                    return "Required";
-                  }
-                  return null;
-                },
-              ),
-              const Padding(padding: EdgeInsets.all(10.0)),
-              InputFormField(
-                textEditingController: controllerJumlahByr,
-                labelText: "Jumlah Pembayaran",
-                hintText: "Masukkan jumlah bayar",
-                hintTextStyle: const TextStyle(fontSize: 15),
-                borderColor: Colors.blue,
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                borderRadius: BorderRadius.circular(10),
-                errorPadding: const EdgeInsets.only(left: 10, top: 10),
-                validator: (v) {
-                  if (v != null && v.isEmpty) {
-                    return "Required";
-                  }
-                  return null;
-                },
-              ),
-              const Padding(padding: EdgeInsets.all(10.0)),
-              InputFormField(
-                textEditingController: controllerBln,
-                labelText: "Bulan",
-                hintText: "Masukkan bulan pembayaran",
-                hintTextStyle: const TextStyle(fontSize: 15),
-                borderColor: Colors.blue,
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                borderRadius: BorderRadius.circular(10),
-                errorPadding: const EdgeInsets.only(left: 10, top: 10),
-                validator: (v) {
-                  if (v != null && v.isEmpty) {
-                    return "Required";
-                  }
-                  return null;
-                },
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    var nmTerima = controllerNamaTerima.text;
-                    var nmSantri = controllerNamaSantri.text;
-                    var jnsByr = controllerJenisByr.text;
-                    var jmlByr = controllerJumlahByr.text;
-                    var bln = controllerBln.text;
-                    void getPDF() async {
-                      final pdf = pw.Document();
-
-                      //buat page
-                      pdf.addPage(
-                        pw.Page(
-                          pageFormat: pd.PdfPageFormat.a4,
-                          build: (pw.Context context) {
-                            return pw.Column(
-                              children: [
-                                pw.Container(
-                                    alignment: pw.Alignment.center,
-                                    color: pd.PdfColors.green600,
-                                    width: double.infinity,
-                                    child: pw.Text("KWITANSI",
-                                        style: pw.TextStyle(
-                                            fontSize: 50,
-                                            fontWeight: pw.FontWeight.bold))),
-                                pw.Container(
-                                    alignment: pw.Alignment.center,
-                                    width: double.infinity,
-                                    child: pw.Text("${DateTime.now()} \n\n\n",
-                                        style:
-                                            const pw.TextStyle(fontSize: 20))),
-                                pw.Container(
-                                    alignment: pw.Alignment.centerLeft,
-                                    width: double.infinity,
-                                    child: pw.Text(
-                                        "\n\n"
-                                        "Penerima : $nmTerima\n\n"
-                                        "Nama Santri : $nmSantri\n\n"
-                                        "Jenis Pembayaran : $jnsByr\n\n"
-                                        "Jumlah Bayar : Rp$jmlByr\n\n"
-                                        "Bulan : $bln\n",
-                                        style:
-                                            const pw.TextStyle(fontSize: 25))),
-                                pw.Container(
-                                    alignment: pw.Alignment.bottomRight,
-                                    width: double.infinity,
-                                    child: pw.Text(
-                                        "\n\n\n\n\n\n\n\n\n\n\n\n\nTTD \n"
-                                        "\n\n"
-                                        "($nmTerima)",
-                                        style:
-                                            const pw.TextStyle(fontSize: 25))),
-                              ],
-                            );
-                          },
-                        ),
-                      ); // Page
-
-                      // simpan pdf
-                      Uint8List bytes = await pdf.save();
-
-                      //buat file kosong di directory
-                      final dir = await getApplicationDocumentsDirectory();
-                      final file = File('${dir.path}/myinvoice.pdf');
-
-                      //timpa file kosong
-                      await file.writeAsBytes(bytes);
-
-                      // buka pdf
-                      await OpenFile.open(file.path);
-                    }
-
-                    getPDF();
-                  },
-                  child: const Text(
-                    "PRINT",
-                  )),
-            ]),
-          ],
+                child: const Text(
+                  "PRINT",
+                )),
+          ]),
         ),
       ),
     );
